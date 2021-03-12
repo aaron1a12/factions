62604786c38903f61e7cc597ba90d57d8cecab36
aaron1a12 <65295020+aaron1a12@users.noreply.github.com>
2021-03-12 18:23:39 -0500

    Added some funcs

--- global.lua
+++ global.lua
@@ -15,6 +15,59 @@ function randomString(length)
     return randomString(length - 1) .. rsCharset[math.random(1, #rsCharset)]
 end
 
+function lerp(pos1, pos2, alpha)
+    return (1-alpha)*pos1 + alpha*pos2
+end
+
+function IsPlayerLookingForward(pPed)
+    if pPed == nil then local pPed = GetPlayerPed(-1) end
+    
+    local camCoords = GetGameplayCamCoord() - GetEntityCoords(pPed)
+    local forward = GetEntityForwardVector(pPed)
+    local dot = (forward.x * camCoords.x) + (forward.y * camCoords.y) + (forward.z * camCoords.z)
+
+    if dot > 0 then return false else return true end
+end
+
+-- Nice enumerator functions. I forgot who wrote this originally but it wasn't me.
+
+local function EnumerateEntities(initFunc, moveFunc, disposeFunc)
+    return coroutine.wrap(function()
+      local iter, id = initFunc()
+      if not id or id == 0 then
+        disposeFunc(iter)
+        return
+      end
+      
+      local enum = {handle = iter, destructor = disposeFunc}
+      setmetatable(enum, entityEnumerator)
+      
+      local next = true
+      repeat
+        coroutine.yield(id)
+        next, id = moveFunc(iter)
+      until not next
+      
+      enum.destructor, enum.handle = nil, nil
+      disposeFunc(iter)
+    end)
+  end
+
+function EnumerateObjects()
+    return EnumerateEntities(FindFirstObject, FindNextObject, EndFindObject)
+end
+
+function EnumeratePeds()
+    return EnumerateEntities(FindFirstPed, FindNextPed, EndFindPed)
+end
+
+function EnumerateVehicles()
+    return EnumerateEntities(FindFirstVehicle, FindNextVehicle, EndFindVehicle)
+end
+
+function EnumeratePickups()
+    return EnumerateEntities(FindFirstPickup, FindNextPickup, EndFindPickup)
+end
 
 --  Event Callback Helpers --------------------------------
 
